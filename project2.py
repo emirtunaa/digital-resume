cfd977144dc1610d7d5dca6ac51d93bf46b7afd2
Emir Tuna Korkmaz <emirtuna@Emirs-MacBook-Pro-2.local>
2023-11-28 12:44:51 +0300

    initial commit

--- project2.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.write("<div id='detailed_description2'></div>", unsafe_allow_html=True)
-
-# Placeholder section for detailed description
-st.header("Project 2")
-st.write("This is the detailed description for project 2.")
